a4254b2e1811ad11041385345984d803f6ff1f57
John Banister <banisterious@gmail.com>
2025-05-30 09:33:45 -0700

    feat: extract applyCustomDateRangeFilter to dedicated CustomDateRangeFilter class

--- src/dom/filters/index.ts
+++ src/dom/filters/index.ts
@@ -7,6 +7,7 @@
 export { FilterUI } from './FilterUI';
 export { FilterControls } from './FilterControls';
 export { DateFilter } from './DateFilter';
-export type { DateRange } from './DateFilter';
 export { DateRangeService } from './date-range/DateRangeService'; 
-export { FilterManager } from './FilterManager'; 
+export { FilterManager } from './FilterManager';
+export { FilterDisplayManager } from './FilterDisplayManager';
+export { CustomDateRangeFilter, type DateRange } from './CustomDateRangeFilter'; 
